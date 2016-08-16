95fe10cbc03bbfb6501bef0611c0524c61c123fe
dotnet centos <dotnet@centos.org>
2016-08-16 01:36:54 -0400

    使用 $http 呼叫後台 WebApi 取出 trips & stops 的資料

--- wwwroot/js/tripEditorController.js
+++ wwwroot/js/tripEditorController.js
@@ -4,8 +4,19 @@
 
     angular.module("app-trips").controller("tripEditorController", tripEditorController);
 
-    function tripEditorController() {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
-        vm.name = "yiming";
+        vm.tripName = $routeParams.tripName;
+        vm.stops = [];
+        vm.errorMessage = "";
+        vm.isbusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+             .then(function(response) {
+                angular.copy(response.data, vm.stops);
+             }, function(error) {
+                 vm.errorMessage = "Failed to load stops";
+             })
+             .finally(function() { vm.isbusy = false; });
     }
 })();
