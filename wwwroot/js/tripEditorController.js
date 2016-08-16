fc572bb7373bb1a78d84029df6c87f13568b8a96
dotnet centos <dotnet@centos.org>
2016-08-16 02:52:41 -0400

    加入 gmap 的支援，將 stops 座標顯示在地圖上

--- wwwroot/js/tripEditorController.js
+++ wwwroot/js/tripEditorController.js
@@ -14,9 +14,29 @@
         $http.get("/api/trips/" + vm.tripName + "/stops")
              .then(function(response) {
                 angular.copy(response.data, vm.stops);
+                _showMap(vm.stops);
              }, function(error) {
                  vm.errorMessage = "Failed to load stops";
              })
-             .finally(function() { vm.isbusy = false; });
+             .finally(function() { vm.isbusy = false; });    
+    }
+
+    function _showMap(stops) {  // 使用 _ 代表此為 inside javascript object scope，不會被外部存取
+        if (stops && stops.length > 0) {
+            var mapStops = _.map(stops, function(item){
+                return {
+                    lat: item.latitude,
+                    long: item.longitude,
+                    info: item.name
+                };
+            });
+            //Show Map
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",   // 指定要顯示的位置: html tag id="#map"
+                currentStop: 1,
+                initialZoom: 3
+            });
+        }
     }
 })();
